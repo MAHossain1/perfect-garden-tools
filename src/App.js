7435ec27b2a0640b0a7a5db51364d6bc5b73d316
MAHossain1 <mahpro110@gmail.com>
2022-05-22 08:24:43 +0600

    react router and navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import MyProfile from "./pages/MyProfile/MyProfile";
+import Navbar from "./pages/Shared/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="myprofile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        gardentheme: {
+          primary: "#1fa139",
+          secondary: "#09e034",
+          accent: "#a1d1ab",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 };
