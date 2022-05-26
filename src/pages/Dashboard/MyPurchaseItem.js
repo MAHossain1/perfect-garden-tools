35de13c047289549dd362a265822e140579c64bb
MAHossain1 <mahpro110@gmail.com>
2022-05-26 18:41:16 +0600

    dashboard slidebar

--- src/pages/Dashboard/MyPurchaseItem.js
+++ src/pages/Dashboard/MyPurchaseItem.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyPurchaseItem = () => {
+  const [purchasedItem, setPurchasedItem] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/purchasing=${user.email}`)
+        .then(res => res.json())
+        .then(data => setPurchasedItem(data));
+    }
+  }, [user]);
+
   return (
     <div>
-      <h2>My Purchase Item</h2>
+      <h2>My Purchase Item {purchasedItem.length}</h2>
     </div>
   );
 };
